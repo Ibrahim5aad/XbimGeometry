78a3ec2a520680183b5ab1a4cc2cd3c7cdb64e57
Ibrahim Saad <ibrahimsaad419@gmail.com>
2026-02-26 12:59:08 +0100

    Switch to dynamic linkage on Linux
    [skip ci]

--- src/Xbim.Geometry.Engine.Native/triplets/release/x64-linux.cmake
+++ src/Xbim.Geometry.Engine.Native/triplets/release/x64-linux.cmake
@@ -1,10 +1,5 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
 set(VCPKG_CMAKE_SYSTEM_NAME Linux)
 set(VCPKG_BUILD_TYPE release)
-
-# Static libs are linked into a .so loaded via dlopen() by .NET.
-# Default initial-exec TLS model exhausts the static TLS block.
-set(VCPKG_C_FLAGS "-ftls-model=global-dynamic")
-set(VCPKG_CXX_FLAGS "-ftls-model=global-dynamic")
